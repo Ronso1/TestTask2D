66949b6f8facf6e887141311863cb5aea66d930a
Ivan Gurev <gureff.al@yandex.ru>
2025-04-22 18:38:31 +0300

    Improve rotate gun logic

--- Assets/Scripts/Gun/GunLogic.cs
+++ Assets/Scripts/Gun/GunLogic.cs
@@ -5,10 +5,12 @@ namespace Gun
 {
     public class GunLogic : MonoBehaviour
     {
+        [Header("Links to references")]
         [SerializeField] private PlayerInput _playerInput;
         [SerializeField] private Camera _camera;
-
-        [SerializeField] private float _offset;
+        [Header("Parameters")]
+        [SerializeField] private float _minClamp;
+        [SerializeField] private float _maxClamp;
 
         private void OnEnable()
         {
@@ -22,10 +24,15 @@ namespace Gun
 
         private void OnWeaponStartRotate(Vector2 touchPosition)
         {
+            const float offset = -90f;
+
             var worldPosition = _camera.ScreenToWorldPoint(touchPosition);
             var direction = worldPosition - transform.position;
 
-            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + _offset;
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + offset;            
+            angle = Mathf.Clamp(angle, _minClamp, _maxClamp);
+
+            if (angle <= _minClamp || angle >= _maxClamp) return;
 
             transform.localRotation = Quaternion.Euler(0f, 0f, angle);
         }
